4552e20f87c68bac7f5276ff02abd5b77a2e963c
majolex <majolex@matts-MacBook-Air.local>
2019-02-03 14:14:53 -0700

    added things

---
+++ bamazonCustomer.js
@@ -0,0 +1,87 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "viviana13",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connection successful!");
+    console.log("");
+    makeTable();
+});
+
+var makeTable = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.table(res);        
+        selectItem(res);
+    })
+};
+
+var selectItem = function(inventory) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'itemChoice',
+        message: 'What is the ID of the item you would like to purchase? [Quit with Q]',
+        validate: function(val) {
+            return !isNaN(val) || val.toLowerCase() === "q"
+        }
+    }]).then(function(val) {
+        // Check if user wants to exit
+        userExit(val.itemChoice);
+
+        var choiceId = parseInt(val.itemChoice);
+        var product = checkInventory(choiceId, inventory);
+
+        if (product) {
+            selectQuantity(product);
+        } else {
+            console.log("That item is not in the inventory")
+        }
+    })
+}
+
+var selectQuantity = function(product) {
+ // Inquirer prompt asking user for quantity desired
+ // Validate to make sure user enters a number or a Q
+ // Check if user wants to exit
+ // Check if quantity desired exceeds quantity available 
+    // var quantity = parseInt(val.itemQuantity) if (quantity > product.stock_quantity)
+ // If quantity is available, run the makePurchase function
+ // If quantity is not available, console.log a message stating that
+}
+
+var makePurchase = function() {
+    // Connect to mysql 
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantity, product.item_id], 
+    function(err, res) {
+        console.log("Congratulations.  Your purchase was successful");
+        makeTable();
+    })
+}
+
+var checkInventory = function(choiceId, inventory) {
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].item_id === choiceId) {
+            return inventory[i];
+        }        
+    }
+    return null;
+}
+
+var userExit = function(choice) {
+    if (choice.toLowerCase() === "q") {
+        console.log("")
+        console.log("Goodbye!");
+        console.log("");
+        process.exit(0)
+    }
+}
+
+    
